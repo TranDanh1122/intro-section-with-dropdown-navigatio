16680e7cfec2f70977facc6d8b89ea4648d1dff5
TranDanh1122 <trandanh14042000@gmail.com>
2024-12-27 22:47:25 +0700

    complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
     extend: {
       colors: {
         "almost_white": "hsl(0, 0%, 98%)",
         "medium_gray": "hsl(0, 0%, 41%)",
         "almost_black": "hsl(0, 0%, 8%)",
+      },
+      screens: {
+        mb: { min: "0", max: "1024px" }
       }
-
     },
   },
   plugins: [],
