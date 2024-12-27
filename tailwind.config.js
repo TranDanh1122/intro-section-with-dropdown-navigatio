61032e35d448df4802ca8dd3e48080ecac8e5b65
TranDanh1122 <trandanh14042000@gmail.com>
2024-12-27 20:07:50 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "almost_white": "hsl(0, 0%, 98%)",
+        "medium_gray": "hsl(0, 0%, 41%)",
+        "almost_black": "hsl(0, 0%, 8%)",
+      }
+
+    },
+  },
+  plugins: [],
+}
+
